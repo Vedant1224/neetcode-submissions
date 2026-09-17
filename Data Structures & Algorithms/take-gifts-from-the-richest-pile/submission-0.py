7beac53b8e2d48e605f0c633c88eb5f63429402a
Vedant <115943849+Vedant1224@users.noreply.github.com>
2026-09-17 15:43:41 -0400

    Add: take-gifts-from-the-richest-pile - submission-0

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        output = 0
+        while k > 0:
+            gifts.sort(reverse = True)
+            gifts[0] = int(gifts[0] ** 0.5)
+            k-=1
+
+        output = sum(gifts)
+        return output
+
+        
